51efc83f5ab5c20aa7bdde776f759176f0f0499c
FluffyTheWise <31629464+FluffyTheWise@users.noreply.github.com>
2019-10-31 20:36:27 +0100

    Add files via upload

--- arrange.py
+++ arrange.py
@@ -26,3 +26,27 @@ def ndarrange(A):
     for i in range (dim):
         A[i] = arrange(A[i])
     return A
+
+def axisarrange(A, B):
+    length = np.size(A)
+    x = 0
+    i = 0
+    temp = 0
+    temp2 = 0
+    
+    while True:
+        if x == length:
+            return A, B
+        if i == length - 1:
+            i = 0
+        if A[i] > A[i + 1]:
+            temp = A[i + 1]
+            temp2 = B[i + 1]
+            A[i + 1] = A[i]
+            B[i + 1] = B[i]
+            A[i] = temp
+            B[i] = temp2
+            x = 0
+        else:
+            x = x + 1
+        i = i + 1
